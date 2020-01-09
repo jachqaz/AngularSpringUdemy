335b4e1e46c57d0b15aced511722f5199da7065c
Johan Castano <jach_1997@hotmail.com>
2020-01-09 09:54:15 -0500

    Añadiendo el método login() en el componente y nueva clase Usuario

--- src/app/usuarios/login/login.component.ts
+++ src/app/usuarios/login/login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Usuario} from "../usuario";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,18 @@ import {Component, OnInit} from '@angular/core';
 export class LoginComponent implements OnInit {
   titulo: string = 'Por favor Sign In!';
 
+  usuario: Usuario;
+
   constructor() {
+    this.usuario = new Usuario();
   }
 
   ngOnInit() {
   }
 
+  login(): void {
+    console.log(this.usuario);
+    if (this.usuario.username == null || this.usuario.password == null)
+      Swal.fire('Error Login', 'Username o password vacia', 'error');
+  }
 }
