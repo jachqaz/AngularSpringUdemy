abad1fd1d6a16df75e27fb6fbb8669f817712d03
Johan Castano <jach_1997@hotmail.com>
2020-01-09 15:01:48 -0500

    Manejando código de error 403 de Accedo Denegado o Prohibido (Forbidden)

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -174,8 +174,13 @@ export class ClienteService {
   }
 
   private isNoAutorizado(e): boolean {
-    if (e.status == 401 || e.status == 403) {
+    if (e.status == 401) {
       this.router.navigate(['/login']);
+      return true
+    }
+    if (e.status == 403) {
+      Swal.fire('Acceso denegado', `Hola ${this.authService.usuario.username} no tienes acceso a este recurso`, 'warning');
+      this.router.navigate(['/clientes']);
       return true
     }
     return false
