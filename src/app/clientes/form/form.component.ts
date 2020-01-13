121e2390ce94483154e5866c3b9812e423b53a8d
Johan Castano <jach_1997@hotmail.com>
2020-01-13 10:48:14 -0500

    Solucionando un problema de recursión al editar el cliente

--- src/app/clientes/form/form.component.ts
+++ src/app/clientes/form/form.component.ts
@@ -55,6 +55,7 @@ export class FormComponent implements OnInit {
   }
 
   update(): void {
+    this.cliente.facturas = null;
     this.clienteService.update(this.cliente).subscribe(cliente => {
         this.router.navigate(['/clientes']);
         swal.fire('Cliente actualizado', `Cliente ${cliente.nombre} actualizado con exito`, 'success');
